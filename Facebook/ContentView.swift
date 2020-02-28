f77eec20df0930886f01dcb2b31d2fb24406cf47
Bomin Chuang <appppm31012@gmail.com>
2020-02-28 14:10:28 +0800

    Initial Commit

---
+++ Facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook
+//
+//  Created by Bomin on 2020/2/28.
+//  Copyright © 2020 Bomin Chuang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
